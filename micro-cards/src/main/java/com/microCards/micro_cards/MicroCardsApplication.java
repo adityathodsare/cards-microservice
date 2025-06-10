8613d6957a04763eb83689adf2443ee1ea3210e3
adityathodsare <thodsareaditya@gmail.com>
2025-06-10 16:05:26 +0530

    Initial commit

---
+++ micro-cards/src/main/java/com/microCards/micro_cards/MicroCardsApplication.java
@@ -0,0 +1,13 @@
+package com.microCards.micro_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCardsApplication.class, args);
+	}
+
+}
---
+++ micro-cards/src/test/java/com/microCards/micro_cards/MicroCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microCards.micro_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
